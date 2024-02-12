0379eaff1da7c3764cef0193301dd86f8b1e912f
Nishkarsh Saxena <94742536+nishkarsh800@users.noreply.github.com>
2024-02-12 13:41:24 +0530

    Update 1.kt

--- coroutines/Chapter 1/1.kt
+++ coroutines/Chapter 1/1.kt
@@ -5,9 +5,9 @@ import kotlin.concurrent.thread
 // code within the thread is executed completely then only our main function ended
 fun main() {
     println("Main starts : ${Thread.currentThread().name}")
-    thread{
+    thread{  // creates a background thread(worker thread)
         println("Fake work starts : ${Thread.currentThread().name}")
-        Thread.sleep(1000)
+        Thread.sleep(1000) // pretending doing some work... maybe file upload
         println("Fake work ends : ${Thread.currentThread().name}")
     }
 
